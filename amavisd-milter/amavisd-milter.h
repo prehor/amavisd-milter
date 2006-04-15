7c3b8a66814cdfc209c9ddec81453883c3caeb49
reho <reho>
2006-04-15 23:22:53 +0000

    MAXAMABUF is increased from 2048 to 65536 because it must be greater then confMAX_HEADERS_LENGTH which is usualy set to 32768.

--- amavisd-milter/amavisd-milter.h
+++ amavisd-milter/amavisd-milter.h
@@ -25,7 +25,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: amavisd-milter.h,v 1.5 2006/01/23 15:15:49 reho Exp $
+ * $Id: amavisd-milter.h,v 1.6 2006/01/25 21:10:26 reho Exp $
  */
 
 #ifndef _AMAVISD_MILTER_H
@@ -35,7 +35,7 @@
 
 /* Maximum message buffers length */
 #define MAXLOGBUF	1024	/* syslog message buffer */
-#define MAXAMABUF	2048	/* amavisd communication buffer */
+#define MAXAMABUF	65536	/* amavisd communication buffer */
 
 struct mlfiCtx;
 
